713cc1fa8d887c7652c63a0f8ca642836b519be1
Spencer King <spencer@dn0a203195.sunet>
2013-01-19 16:18:17 -0800

    stuff

--- p2/p2/js/tetris.js
+++ p2/p2/js/tetris.js
@@ -11,6 +11,10 @@ $(document).ready(function() {
     }
 });
 
+var total_pixels = 0;
+var matching_pixels = 0;
+var pass_percentage = 0.93;
+
 /*
  * In this example, we show you how to overlay the shadow information over
  * an image painted into the canvas. This function is called in a loop
@@ -40,7 +44,18 @@ function renderShadow() {
         // IMPORTANT: make sure that the width and height of your two
         // canvases match. Otherwise, here be dragons!
         for(var i = 0; i < shadow.data.length; i=i+4) {
-            // i = red; i+1 = green; i+2 = blue; i+3 = alpha
+		total_pixels++;
+            	console.log("r:" + pixels.data[i] + " g:" + pixels.data[i+1] + " b:" + pixels.data[i+2]);
+		if(pixels.data[i] < 245 && pixels.data[i+1] < 245 && pixels.data[i+2] < 245) {
+		if (shadow.data[i] != OVERLAY && shadow.data[i+1] != OVERLAY && shadow.data[i+2] != OVERLAY) {
+			matching_pixels++;
+		}
+	    } else {
+		if (shadow.data[i] == OVERLAY && shadow.data[i+1] == OVERLAY && shadow.data[i+2] == OVERLAY) {
+			matching_pixels++;
+		} 
+		}
+	    // i = red; i+1 = green; i+2 = blue; i+3 = alpha
             if(shadow.data[i] == OVERLAY && shadow.data[i+1] == OVERLAY && shadow.data[i+2] == OVERLAY) {
                 // If the current shadow pixel is to be overlayed, copy it over to
                 // our canvas' pixel data
@@ -49,7 +64,11 @@ function renderShadow() {
                 pixels.data[i+2] = shadow.data[i+2];
             }
         }
-
+	console.log("yoo");
+	console.log(matching_pixels/total_pixels);
+	if ((matching_pixels/total_pixels) >= pass_percentage){
+		console.log("done. On to next tetris block");
+	}
         // And now, paint our pixels array back to the canvas.
         shadowContext.putImageData(pixels, 0, 0);
     }
