61f650a43560b2b80e62a0283f42b33e21443af3
Spencer King <spencer@rescomp-12-249534.stanford.edu>
2013-01-21 04:49:07 -0800

    new stuff

--- p2/p2/js/tetris.js
+++ p2/p2/js/tetris.js
@@ -1,6 +1,41 @@
-var IMG_SRC  = 'media/tetristest.jpg';
+var c1, c2, c3, ID = null;
+var time = null;
+
+function startTimer() {
+    if (ID != null) {
+        stopTimer();
+    }
+    c1 = 0;
+    c2 = 0;
+    c3 = 0;
+    ID = window.setInterval(run, 1000);
+}
+
+function stopTimer() {
+    window.clearInterval(ID);
+    ID = null;
+}
+
+function run() {
+    c1++;
+    if (c1 == 60) {
+        c1 = 0;
+        c2++;
+        if (c2 == 60) {
+            c2 = 0;
+        }
+    }
+
+    var o1 = (c1 <= 9 ? "0" : "") + c1;
+    var o2 = (c2 <= 9 ? "0" : "") + c2;
+    var o3 = (c3 <= 9 ? "0" : "") + c3;
+    document.getElementById("time").innerHTML = o3 + ":" + o2 + ":" + o1;
+}
+
+
+var IMG_SRC  = 'media/testris2.jpg';
 var OVERLAY  = 255;   // 0 = foreground, 255 = background
-var imageArr = new Array ('media/tetristest.jpg', 'media/tetris_block.jpg');
+var imageArr = new Array ('media/testris2.jpg', 'media/fingerpuppettest.jpg');
 var index = 0;
 
 var tetrisImage;
@@ -15,7 +50,7 @@ $(document).ready(function() {
 
 var total_pixels = 0;
 var matching_pixels = 0;
-var pass_percentage = 0.9;
+var pass_percentage = 0.93;
 
 /*
  * In this example, we show you how to overlay the shadow information over
@@ -47,16 +82,19 @@ function renderShadow() {
         // IMPORTANT: make sure that the width and height of your two
         // canvases match. Otherwise, here be dragons!
         for(var i = 0; i < shadow.data.length; i=i+4) {
-		total_pixels++;
-		if(pixels.data[i] > 245 &&  pixels.data[i+1] < 10  && pixels.data[i+2] < 10) {
-		if (shadow.data[i] == OVERLAY && shadow.data[i+1] == OVERLAY && shadow.data[i+2] == OVERLAY) {
-			matching_pixels++;
-		}
-	    } else {
-		if (shadow.data[i] != OVERLAY && shadow.data[i+1] != OVERLAY && shadow.data[i+2] != OVERLAY) {
-			matching_pixels++;
-		} 
-		}
+    		total_pixels++;
+
+            //if image pixel is red, then check if corresponding pixel of shadow is white
+    		if(pixels.data[i] > 245 &&  pixels.data[i+1] < 10 && pixels.data[i+2] < 10) {
+                if (shadow.data[i] == OVERLAY && shadow.data[i+1] == OVERLAY && shadow.data[i+2] == OVERLAY) {
+                    matching_pixels++;
+                } 
+    	    } else {//else check if corresponding pixel of shadow is not white
+
+                if (shadow.data[i] != OVERLAY && shadow.data[i+1] != OVERLAY && shadow.data[i+2] != OVERLAY) {
+                    matching_pixels++;
+                }
+    		}
 	    // i = red; i+1 = green; i+2 = blue; i+3 = alpha
             if(shadow.data[i] == OVERLAY && shadow.data[i+1] == OVERLAY && shadow.data[i+2] == OVERLAY) {
                 // If the current shadow pixel is to be overlayed, copy it over to
@@ -66,15 +104,22 @@ function renderShadow() {
                 pixels.data[i+2] = shadow.data[i+2];
             }
         }
-	console.log("yoo");
-	console.log(matching_pixels/total_pixels);
+    var scoreDisplay = document.getElementById("score-display");
+    var score = Math.round(100*matching_pixels/total_pixels);
+    scoreDisplay.innerHTML = " " + score + "%";
 	if ((matching_pixels/total_pixels) >= pass_percentage){
-		console.log("done. On to next tetris block");
-    		if (index < (imageArr.length - 1)){
+    	if (index < (imageArr.length - 1)){
+            alert("Congratulations! You passed the level! On to next image...");
+            alert("Please move yourself out of image before moving to next image.")
 			index++;
 			tetrisImage = new Image();
-    			tetrisImage.src = imageArr[index];
-		}
+    		tetrisImage.src = imageArr[index];
+		}else{
+            alert("Congratulations, you finished all levels!")
+            stopTimer();
+            alert("You finished in " + document.getElementById("time").innerHTML)
+            location.reload();
+        }
 	}
         // And now, paint our pixels array back to the canvas.
         shadowContext.putImageData(pixels, 0, 0);
